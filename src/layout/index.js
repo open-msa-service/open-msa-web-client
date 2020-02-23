0312060cfe1bd065bef0b1afb4d6e3495040b9a7
jongminelee0 <jongminlee0@gmail.com>
2020-02-24 00:14:21 +0900

    init commit

---
+++ src/layout/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+
+
+class Layout extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            showLoginForm : false
+        }
+    }
+
+    render(){
+        const {showLoginForm} = this.state;
+
+        return (
+            
+        )
+    }
+}
